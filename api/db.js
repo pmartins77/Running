cee8421b348966d795d16a4b933dad44408ae340
pmartins77 <140052720+pmartins77@users.noreply.github.com>
2025-02-22 18:28:30 +0100

    db.js

--- api/db.js
+++ api/db.js
@@ -8,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }  // ⚠️ Important pour NeonDB
+    ssl: { rejectUnauthorized: false }  // ✅ Important pour NeonDB
 });
 
 pool.on("connect", () => {
@@ -19,4 +19,4 @@ pool.on("error", (err) => {
     console.error("❌ Erreur avec la base PostgreSQL :", err);
 });
 
-module.exports = pool; // ✅ On exporte directement `pool` sans accolades
+module.exports = pool; // ✅ On exporte `pool` directement
