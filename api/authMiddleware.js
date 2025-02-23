47c825547634b4ead8052e5f455a9421adac8a65
pmartins77 <140052720+pmartins77@users.noreply.github.com>
2025-02-23 20:00:32 +0100

    authMiddleware.js

--- api/authMiddleware.js
+++ api/authMiddleware.js
@@ -4,19 +4,16 @@ const SECRET_KEY = process.env.JWT_SECRET || "supersecretkey123";
 
 module.exports = function (req, res, next) {
     const authHeader = req.headers["authorization"];
-
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        console.error("❌ AuthMiddleware : Token manquant ou mal formaté.");
         return res.status(403).json({ error: "Accès interdit. Token manquant ou invalide." });
     }
 
-    const token = authHeader.split(" ")[1]; // Extraction du token
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, SECRET_KEY, (err, decoded) => {
         if (err) {
-            console.error("❌ AuthMiddleware : Token invalide.");
             return res.status(403).json({ error: "Token invalide." });
         }
-        req.userId = decoded.userId; // ✅ Ajout de l'ID utilisateur à la requête
+        req.userId = decoded.userId;
         next();
     });
 };
